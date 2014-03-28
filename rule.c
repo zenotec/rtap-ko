1d2f30fdf0f5e1d37b3a87a76a475f4a27efcbb9
Kevin Mahoney <kevin.mahoney@zenotec.net>
2014-03-27 20:59:00 -0600

    Implementing filter proc filesystem interface

--- rule.c
+++ rule.c
@@ -36,18 +36,9 @@
 // Global variables
 //*****************************************************************************
 
-static const uint8_t mys4[] = { 0xF0, 0x25, 0xB7, 0x00, 0xB5, 0x33 };
-static const uint8_t bc[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-static const uint16_t beacon = { IEEE80211_STYPE_BEACON };
-static const uint16_t probe = { IEEE80211_STYPE_PROBE_REQ };
 
 rule_t ruletbl[] =
 {
-    {RULE_ID_MAC_SA, RULE_CMD_FORWARD, &rtap_filter_80211_mac, (void *)&mys4}, // TODO: temp rule for testing
-    {RULE_ID_MAC_FCTL, RULE_CMD_FORWARD, &rtap_filter_80211_mac, (void *)&probe}, // TODO: temp rule for testing
-    {RULE_ID_MAC_FCTL, RULE_CMD_DROP, &rtap_filter_80211_mac, (void *)&beacon}, // TODO: temp rule for testing
-    {RULE_ID_MAC_DA, RULE_CMD_DROP, &rtap_filter_80211_mac, (void *)&bc}, // TODO: temp rule for testing
-    {RULE_ID_DROP, RULE_CMD_DROP, &rtap_filter_drop, 0 }, // Must be last
     { 0 } // Safety stop
 };
 
